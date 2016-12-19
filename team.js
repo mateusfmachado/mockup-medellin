a82ef744d829dbd62a62f532376b1d3a78798bb0
Mateus Machado <mateusmachado@iftm.edu.br>
2016-12-19 14:27:31 -0200

    first version

---
+++ team.js
@@ -0,0 +1,307 @@
+const team = {
+	name: "Deportivo independiente medellin",
+	year: "1913",
+	country: "Colombia",
+	phone:"+57 (4) 444 6789",
+	email: "info@dim.com.co",
+	venue: {
+		name:"Estadio Atanasio Girardot",
+		city: "Medellin",
+		capacity: 45943
+	},
+	trophies: {
+		primeraA: 5,
+		copaColombia: 1
+	},
+	primeraA:{
+		_2016: {
+			rank: 5,
+			matchesPlayed: 20,
+			wins: 9,
+			draws: 6,
+			loses: 5,
+			goals: {
+				for: 31,
+				against: 26,
+				diff: 5
+			},
+			points: 33
+		}
+	},
+	squad: {
+		goalkeepers: [
+			{
+				name: "L. Vasquez",
+				_2016: {
+					appearances:3,
+					goals: 0,
+				},				
+				country: "Colombia"
+			},
+			{
+				name: "A. Quintana",
+				_2016: {
+					appearances:0,
+					goals: 0,
+				},				
+				country: "Colombia"
+			},
+			{
+				name: "D. Gonzalez",
+				_2016: {
+					appearances:45,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+		],
+		defenders: [
+			{
+				name: "H. Pertuz",
+				_2016: {
+					appearances:28,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "A. Mosquera",
+				_2016: {
+					appearances:25,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "M. Piedrahita",
+				_2016: {
+					appearances:13,
+					goals: 2,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "J. Arias",
+				_2016: {
+					appearances:15,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "L. Tipton",
+				_2016: {
+					appearances:21,
+					goals: 2,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "C. Saiz",
+				_2016: {
+					appearances:33,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "E. Cordoba",
+				_2016: {
+					appearances:4,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "W. Parra",
+				_2016: {
+					appearances:17,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+		],
+		midfielders: [
+			{
+				name: "J. Valencia",
+				_2016: {
+					appearances:13,
+					goals: 1,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "J. Hernandez",
+				_2016: {
+					appearances: 38,
+					goals: 4,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "F. Burbano",
+				_2016: {
+					appearances:12,
+					goals: 1,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "C. Marrugo",
+				_2016: {
+					appearances:39,
+					goals: 11,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "S. Macias",
+				_2016: {
+					appearances:15,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "E. Atuesta",
+				_2016: {
+					appearances: 4,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "J. Giraldo",
+				_2016: {
+					appearances: 0,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "D. Moreno",
+				_2016: {
+					appearances: 23,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "F. Santa",
+				_2016: {
+					appearances: 2,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "J. Cabezas",
+				_2016: {
+					appearances:29,
+					goals: 2,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "M. Molina",
+				_2016: {
+					appearances: 39,
+					goals: 9,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "E. Valencia",
+				_2016: {
+					appearances:8,
+					goals: 1,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "M. Cortes",
+				_2016: {
+					appearances:10,
+					goals: 5,
+				},
+				country: "Colombia"
+			}
+		],
+		attackers: [
+			{
+				name: "L. Arias",
+				_2016: {
+					appearances: 39,
+					goals: 5,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "Yorleys Mena",
+				_2016: {
+					appearances: 4,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "H. Hechalar",
+				_2016: {
+					appearances: 40,
+					goals: 6,
+				},
+				country: "Argentina"
+			},
+			{
+				name: "N. Carmona",
+				_2016: {
+					appearances: 3,
+					goals: 0,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "J. Caicedo",
+				_2016: {
+					appearances:38,
+					goals: 9,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "C. Ibarguen",
+				_2016: {
+					appearances: 4,
+					goals: 1,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "L. Castro",
+				_2016: {
+					appearances: 29,
+					goals: 12,
+				},
+				country: "Colombia"
+			},
+			{
+				name: "A. Cabras",
+				_2016: {
+					appearances: 0,
+					goals: 0,
+				},
+				country: "Colombia"
+			}
+		],
+		coach: [
+			{
+				name: "L. Alvarez",
+				country: "Colombia"
+			},
+			{
+				name: "H. Martinez",
+				country: "Colombia"
+			}
+		]
+	}
+};

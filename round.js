fe04ed2a7e6514279889524d9d5ea8f5a64b3be0
Mateus Machado <mateusmachado@iftm.edu.br>
2016-12-20 14:09:07 -0200

    added one match for 19 round

---
+++ round.js
@@ -0,0 +1,306 @@
+const round = {
+	number: 19,
+	competition: "Primera A",
+	dates : [
+		"11/04/16",
+		"11/05/16",
+		"11/06/16",
+		"11/07/16"
+	],
+	matches: [
+		"11/04/16": [
+			{
+				kickOff: "22:45",
+				venue: "Estadio de La Independencia (Tunja)",
+				attendance: 2600,
+				goals: {
+					teamA: [],
+					teamB: [
+						{
+							time: 16,
+							player: "J. Valencia",
+							team: "Once Caldas"
+						}
+					]
+				},
+				cards: {
+					yellow: {
+						teamA: [
+							{
+								player: "N. Carreno",
+								time: 33
+							},
+							{
+								player: "J. Murillo",
+								time: 85
+							},
+							{
+								player: "D. Rosero Valencia",
+								time: 10
+							},
+							{
+								player: "K. Rendon",
+								time: 84
+							},
+						],
+						teamB: [
+							{
+								player: "J. Moreno",
+								time: 40
+							},
+							{
+								player: "J. Valencia",
+								time: 56
+							}						
+						]
+					},
+					red: []
+				},
+				additionalInfo: {
+					referee: {
+						name: "E. Vergara",
+						country: "Colombia"
+					},
+					assistants: [
+						{
+							name: "A. Guzman",
+							country: "Colombia"
+						},
+						{
+							name: "G. Padilla",
+							country: "Colombia"
+						}
+					],
+					fourthOfficial: {
+						name: "V. Wilches",
+						country:"Colombia"
+					}
+				},
+				gameStats: {
+					corners: {
+						teamA: 10,
+						teamB: 2
+					},
+					shotsOnTarget: {
+						teamA: 8,
+						TeamB: 3
+					},
+					shotsWide: {
+						teamA: 8,
+						teamB: 3
+					},
+					fouls: {
+						teamA: 11,
+						teamB: 18
+					},
+					offsides: {
+						teamA: 3,
+						teamB: 2
+					},
+					possession: {
+						teamA: 62,
+						teamB: 38
+					}
+				},
+				teamA: {
+					name: "Patriotas Boyaca",
+					lineups: [
+						{
+							number: 25,
+							name: "A. Otero"
+						},
+						{
+							number: 20,
+							name: "J. Cano"
+						},
+						{
+							number: 14,
+							name: "N. Carreno"
+						},
+						{
+							number: 2,
+							name: "J. Murillo"
+						},
+						{
+							number: 19,
+							name: "D. Rosero Valencia"
+						},
+						{
+							number: 11,
+							name: "M. Gomez"
+						},
+						{
+							number: 3,
+							name: "A. Zapata"
+						},
+						{
+							number: 5,
+							name: "L. Vasquez"
+						},
+						{
+							number: 6,
+							name: "R. Loaiza"
+						},
+						{
+							number: 18,
+							name: "E. Ibarguen"
+						},
+						{
+							number: 17,
+							name: "D. Alvarez"
+						},
+					],
+					coaches: [
+						"H. Rivera"
+					],
+					substitutes: [
+						{
+							number: 8,
+							name: "U. Luna",
+							enter: {
+								time: 46,
+								player: "E. Ibarguen"
+							}
+						},
+						{
+							number: 10,
+							name: "S. Villota",
+							enter: {
+								time: 60,
+								player: "L. Vasquez"
+							}
+						},
+						{
+							number: 7,
+							name: "K .Rendon",
+							enter: {
+								time: 76,
+								player: ". Cano"
+							}
+						},
+						{
+							number: 1,
+							name: "J. Castillo",
+							enter: {}
+						},
+						{
+							number: 9,
+							name: "D. Herrera",
+							enter: {}
+						},
+						{
+							number: 21,
+							name: "C. Rodriguez",
+							enter: {}
+						},
+						{
+							number: 24,
+							name: "D. Parra",
+							enter: {}
+						},
+					]
+				},
+				teamB: {
+					name: "Once Caldas",
+					lineups: [
+						{
+							number: 12,
+							name: "J. Cuadrado"
+						},
+						{
+							number: 4,
+							name: "J. De La Cuesta"
+						},
+						{
+							number: 15,
+							name: "L. Murillo"
+						},
+						{
+							number: 27,
+							name: "J. Lopera"
+						},
+						{
+							number: 14,
+							name: "D. Gomez"
+						},
+						{
+							number: 3,
+							name: "J. Moreno"
+						},
+						{
+							number: 16,
+							name: "E. Soto"
+						},
+						{
+							number: 22,
+							name: "C. Quintero"
+						},
+						{
+							number: 17,
+							name: "J. Viveros"
+						},
+						{
+							number: 20,
+							name: "J. Valencia"
+						},
+						{
+							number: 24,
+							name: "D. Cure"
+						},
+					],
+					coaches: [
+						"W. Cifuentes",
+						"H. Lisi"
+					],
+					substitutes: [
+						{
+							number: 9,
+							name: "S. Romero",
+							enter: {
+								time: 59,
+								player: "J. Valencia"
+							}
+						},
+						{
+							number: 23,
+							name: "J. Noy",
+							enter: {
+								time: 59,
+								player: "E. Soto"
+							}
+						},
+						{
+							number: 26,
+							name: "O. Estupinan",
+							enter: {
+								time: 80,
+								player: "C. Quintero"
+							}
+						},
+						{
+							number: 2,
+							name: "A. Zaluaga",
+							enter: {}
+						},
+						{
+							number: 21,
+							name: "J. Mosquera",
+							enter: {}
+						},
+						{
+							number: 25,
+							name: "Sergio Roman",
+							enter: {}
+						},
+						{
+							number: 28,
+							name: "D. Lemos",
+							enter: {}
+						},
+					]
+				}
+			},
+			
+		]
+	]	
+};
